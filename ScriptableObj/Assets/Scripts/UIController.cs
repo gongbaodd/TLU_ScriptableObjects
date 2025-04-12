0e687641fedfbc9b605ecfeee4a2e41e21ee125c
gongbaodd@ProArt <gongbaodd@qq.com>
2025-04-12 22:37:16 +0300

    set click event

--- ScriptableObj/Assets/Scripts/UIController.cs
+++ ScriptableObj/Assets/Scripts/UIController.cs
@@ -11,6 +11,8 @@ public class UIController : MonoBehaviour
     UIDocument uiDocument; // Reference to the UI document
     List<Button> selectors;
 
+    TemplateContainer disabledSelector;
+
     IEnumerator PlaySprite(VisualElement portrait, Sprite[] sprites)
     {
         int index = 0;
@@ -22,34 +24,51 @@ public class UIController : MonoBehaviour
         }
     }
 
+    TemplateContainer CreateSelector(CharacterConfig config)
+    {
+        var selector = selectorTree.CloneTree();
+        selector.AddToClassList("item-template");
+
+        var nameLabel = selector.Q<Label>("name");
+        var descriptionLabel = selector.Q<Label>("description");
+        var strengthLabel = selector.Q<Label>("strength");
+        var charismaLabel = selector.Q<Label>("charisma");
+        var intelligenceLabel = selector.Q<Label>("intelligence");
+        var knowledgeLabel = selector.Q<Label>("knowledge");
+        var portrait = selector.Q<VisualElement>("portrait");
+
+        nameLabel.text = config.name;
+        descriptionLabel.text = config.description;
+        strengthLabel.text = config.strength.ToString();
+        charismaLabel.text = config.charisma.ToString();
+        intelligenceLabel.text = config.intelligence.ToString();
+        knowledgeLabel.text = config.knowledge.ToString();
+        portrait.style.backgroundImage = new StyleBackground(config.Portrait[0]);
+
+        StartCoroutine(PlaySprite(portrait, config.Portrait));
+
+        return selector;
+    }
+
     void InitSelectors()
     {
         var selectionPanel = uiDocument.rootVisualElement.Q<VisualElement>("left");
 
         foreach (var config in characterConfigs)
         {
-            var selector = selectorTree.CloneTree();
-            selector.AddToClassList("item-template");
-
-            var nameLabel = selector.Q<Label>("name");
-            var descriptionLabel = selector.Q<Label>("description");
-            var strengthLabel = selector.Q<Label>("strength");
-            var charismaLabel = selector.Q<Label>("charisma");
-            var intelligenceLabel = selector.Q<Label>("intelligence");
-            var knowledgeLabel = selector.Q<Label>("knowledge");
-            var portrait = selector.Q<VisualElement>("portrait");
-
-            nameLabel.text = config.name;
-            descriptionLabel.text = config.description;
-            strengthLabel.text = config.strength.ToString();
-            charismaLabel.text = config.charisma.ToString();
-            intelligenceLabel.text = config.intelligence.ToString();
-            knowledgeLabel.text = config.knowledge.ToString();
-            portrait.style.backgroundImage = new StyleBackground(config.Portrait[0]);
-
-            StartCoroutine(PlaySprite(portrait, config.Portrait));
-
+            var selector = CreateSelector(config);
             selectionPanel.Add(selector);
+
+            selector.RegisterCallback<ClickEvent>(evt =>
+            {
+                disabledSelector?.SetEnabled(true);
+                selector.SetEnabled(false);
+                disabledSelector = selector;
+
+                var characterPanel = uiDocument.rootVisualElement.Q<VisualElement>("right");
+                characterPanel.Clear();
+                characterPanel.Add(CreateSelector(config));
+            });
         }
     }
 
